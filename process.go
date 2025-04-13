adee456987497b7a15e05b8de036fe1fecc9d552
Pierre Van Remoortere <pierre.van.remoortere@gmail.com>
2025-04-13 17:23:37 +0200

    feat: prcman process

--- process.go
+++ process.go
@@ -16,14 +16,19 @@ type ProcessDefinition struct {
 	Env map[string]string `json:"env"`
 }
 
-func (proc *ProcessDefinition) start() (*os.Process, error) {
+type ProcmanProcess struct {
+	osProc     *os.Process
+	definition *ProcessDefinition
+}
+
+func (proc *ProcmanProcess) start() error {
 	// Implement the logic to start the process
 	// This is a placeholder implementation
-	strEnv := make([]string, 0, len(proc.Env))
-	for k, v := range proc.Env {
+	strEnv := make([]string, 0, len(proc.definition.Env))
+	for k, v := range proc.definition.Env {
 		strEnv = append(strEnv, k+"="+v)
 	}
-	osProc, err := os.StartProcess(proc.Cmd, proc.Args, &os.ProcAttr{
+	osProc, err := os.StartProcess(proc.definition.Cmd, proc.definition.Args, &os.ProcAttr{
 		Env: strEnv,
 		Files: []*os.File{
 			os.Stdin,  // Standard input
@@ -32,9 +37,25 @@ func (proc *ProcessDefinition) start() (*os.Process, error) {
 		},
 	})
 	if err != nil {
-		log.Printf("Error starting process %s: %v", proc.Name, err)
-		return &os.Process{}, err
+		log.Printf("Error starting process %s: %v", proc.definition.Name, err)
+		return err
 	}
-	log.Printf("Started process %s with PID %d", proc.Name, osProc.Pid)
-	return osProc, nil
+	proc.osProc = osProc
+	log.Printf("Started process %s with PID %d", proc.definition.Name, osProc.Pid)
+	return nil
+}
+
+func (proc *ProcmanProcess) wait() {
+	// Implement the logic to wait for the process to finish
+	// This is a placeholder implementation
+	if proc == nil {
+		log.Println("Process is nil, cannot wait")
+		return
+	}
+	state, err := proc.osProc.Wait()
+	if err != nil {
+		log.Printf("Error waiting for process %d: %v", proc.osProc.Pid, err)
+		return
+	}
+	log.Printf("Process %d finished with state: %v", proc.osProc.Pid, state)
 }
