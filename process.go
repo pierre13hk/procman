d2fdb1c990063ec947691801628cdadae0a5750b
Pierre Van Remoortere <pierre.van.remoortere@gmail.com>
2025-04-13 17:32:52 +0200

    feat: manage procman process

--- process.go
+++ process.go
@@ -45,17 +45,42 @@ func (proc *ProcmanProcess) start() error {
 	return nil
 }
 
-func (proc *ProcmanProcess) wait() {
+func (proc *ProcmanProcess) wait() int {
 	// Implement the logic to wait for the process to finish
 	// This is a placeholder implementation
 	if proc == nil {
 		log.Println("Process is nil, cannot wait")
-		return
+		return -1
 	}
 	state, err := proc.osProc.Wait()
+	pid := proc.osProc.Pid
+	proc.osProc = nil
 	if err != nil {
-		log.Printf("Error waiting for process %d: %v", proc.osProc.Pid, err)
+		log.Printf("Error waiting for process %d: %v", pid, err)
+		// creates dangling process?
+		return -1
+	}
+	log.Printf("Process %d finished with state: %v", pid, state)
+	return state.ExitCode()
+}
+
+func (proc *ProcmanProcess) manage() {
+	// Implement the logic to manage the process
+	if proc == nil {
+		log.Println("Process is nil, cannot manage")
 		return
 	}
-	log.Printf("Process %d finished with state: %v", proc.osProc.Pid, state)
+	log.Printf("Managing process %s", proc.definition.Name)
+	for {
+		if proc.osProc == nil {
+			proc.start()
+		}
+		exitCode := proc.wait()
+		if exitCode != 0 {
+			log.Printf("Process %s exited with code %d, restarting...", proc.definition.Name, exitCode)
+		} else {
+			log.Printf("Process %s exited normally, not restarting", proc.definition.Name)
+			break
+		}
+	}
 }
